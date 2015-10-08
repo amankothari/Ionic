1e2e9d5c1aded77fb9ff81fbc6d24a0dd15f661f
aman kothari <amankothari85@gmail.com>
2015-10-08 18:18:26 +0530

    r1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,13 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function ($scope, $ionicModal, $timeout, authService, localStorageService, $location, $ionicHistory, $rootScope, notification, $ionicSideMenuDelegate, $window,$cordovaGeolocation, weatherService) {
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout, authService, localStorageService, $location, $ionicHistory, $rootScope, notification, $ionicSideMenuDelegate, $window, weatherService) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
     // listen for the $ionicView.enter event:
-    alert('hello');
+   
     $scope.weatherload = false;
-    CallEveryTime();
+   // CallEveryTime();
     
     $scope.showEmp = true;
     $scope.authentication = {};
