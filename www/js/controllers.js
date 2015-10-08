aba5beb6ffb0bd19899d982ccc0c7597e3fb8f34
aman kothari <amankothari85@gmail.com>
2015-10-08 15:54:01 +0530

    hello

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,7 @@ angular.module('starter.controllers', [])
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
     // listen for the $ionicView.enter event:
+    alert('hello');
     $scope.weatherload = false;
     CallEveryTime();
     
